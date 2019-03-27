fc5a906acdeeb1421b13855dd8c5c24fa580b0d6
TANVI DHARSANDIA <tanvi.nirma@gmail.com>
2019-03-26 21:09:40 -0500

    Java Prog for Hello Print

---
+++ src/base/HelloPrint.java
@@ -0,0 +1,11 @@
+package base;
+
+public class HelloPrint {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World! This is program is executed from Jenkins.");
+		System.out.println("This line is added after build in Jenkins...");
+	}
+
+}
