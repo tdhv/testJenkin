1818fbffd95b5776c41852a415ad6898b3f196cd
tdhv <40770146+tdhv@users.noreply.github.com>
2019-03-27 17:24:34 -0500

    Update HelloPrint.java
    instead of mul changed to add

--- src/base/HelloPrint.java
+++ src/base/HelloPrint.java
@@ -1,16 +1,15 @@
 package base;
 
 public class HelloPrint {
-
+	
 	public static void main(String[] args) {
 		
 		int total = 0;
 		for(int i=1;i<11;i++) {
-			System.out.println(i + " * 2 = " + i*2);
-			total += (i*2);
+			System.out.println(i + " + 2 = " + i+2);
 		}	
 
 		System.out.println("Total = " + total);
 	}
-
+	
 }
